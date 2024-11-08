02b4e111ff1e470c5114b82a506aac762a733c56
devgugga <gustavogomes90104@gmail.com>
2024-11-08 10:31:18 -0300

    :sparkles: Load dotenv
    Neste commit foi configurado a aplicação para carregar os arquivos .env

--- src/main/java/br/com/ryuu/ryuu/RyuuApplication.java
+++ src/main/java/br/com/ryuu/ryuu/RyuuApplication.java
@@ -1,5 +1,6 @@
 package br.com.ryuu.ryuu;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RyuuApplication {
 
     public static void main(String[] args) {
+        // Load environment variables
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
+
+        // Set environment variables as system properties
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
         SpringApplication.run(RyuuApplication.class, args);
     }
 
