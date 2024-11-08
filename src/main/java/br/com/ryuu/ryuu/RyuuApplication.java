80072ae5514448867ff074032c0b6f879dde6c25
devgugga <gustavogomes90104@gmail.com>
2024-11-08 09:30:51 -0300

    :tada: Initial Commit
    Neste commit foi criado a estrutura inicial do projeto.

---
+++ src/main/java/br/com/ryuu/ryuu/RyuuApplication.java
@@ -0,0 +1,13 @@
+package br.com.ryuu.ryuu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyuuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyuuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ryuu/ryuu/RyuuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ryuu.ryuu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyuuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
